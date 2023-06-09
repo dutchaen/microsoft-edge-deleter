577c8dd2e55f7da574ea4ba8413b1739b58aabe0
dam <127336265+dutchaen@users.noreply.github.com>
2023-06-09 16:14:16 -0700

    Update script.ps1
    better comments

--- script.ps1
+++ script.ps1
@@ -1,23 +1,26 @@
-# list of directories where microsoft edge programs could be located 
+# this is a list of directories where microsoft edge programs could be located 
 $paths = @("C:\Program Files (x86)\Microsoft\Edge\Application", "C:\Program Files (x86)\Microsoft\EdgeCore");
 
-# for every single one
+# and for every single one of them
 foreach ( $path in $paths ) {
 
-    # we check this directory exists, if it doesnt we skip. if it does then we continue
+    # we check if the directory is valid, if it is not valid, we skip this directory. but if it is valid...
     if ([System.IO.Directory]::Exists($path)) {
 
-        # we get the directories of this path
+        # then we get the subdirectories of this directory and for every single subdirectory...
         foreach( $dir in (Get-Item $path).GetDirectories() ) {
-            # we check if the directory contains a version number, ex: 92.0.902.67
-            # if it does have a version number, we add an additional string to the end of directory path to direct it to the edge setup.exe
+            # we check if the subdirectory contains a version number, ex: 92.0.902.67
+            # if it does not have a version number, we skip this subdirectory
+            # but if it does have a version number, we add an additional string to the end of the full subdirectory path so it points to the microsoft edge setup.exe
+            # ex: 'C:\Program Files (x86)\Microsoft\Edge\Application\92.0.902.67' + '\Installer\setup.exe' = 'C:\Program Files (x86)\Microsoft\Edge\Application\92.0.902.67\Installer\setup.exe'
+            
           if ( $dir.FullName -match '(\d{1,4})\.(\d{1,4})\.(\d{1,4})\.(\d{1,4})' ) {
                $setup_path = $dir.FullName + '\Installer\setup.exe';
 
-               # we tell the microsoft edge setup program to uninstall itself from the system
+               # then we tell the microsoft edge setup program to uninstall itself from the system
                Start-Process -FilePath $setup_path -ArgumentList @('--uninstall', '--force-uninstall', '--system-level') -NoNewWindow -Wait;
 
-               # then we actually remove all of the files of microsoft edge
+               # then we actually remove all of the files of microsoft edge from this directory
                Remove-Item -LiteralPath $path -Force -Recurse;
                break
           };
@@ -29,5 +32,5 @@ foreach ( $path in $paths ) {
 Remove-Item -LiteralPath 'C:\Program Files (x86)\Microsoft\Edge' -Force -Recurse;
 Remove-Item -LiteralPath 'C:\Program Files (x86)\Microsoft\EdgeUpdate' -Force -Recurse;
 
-# then we are done :D
+# and then we are done :D
 'Deleted Microsoft Edge from this computer.'
